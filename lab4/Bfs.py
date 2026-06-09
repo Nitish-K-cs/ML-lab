0ca8abcb6fbfa05ad20e334d9f936f86f5383b32
Nitish K <nitish258k@gmail.com>
2026-06-09 09:40:45 +0530

    Refactor BFS implementation using deque

--- lab4/Bfs.py
+++ lab4/Bfs.py
@@ -1,51 +1,44 @@
-from queue import PriorityQueue
+from collections import deque
 
 graph = {
-    'A' : [('B' , 4) , ('C' , 3)],
-    'B' : [('D' , 5) , ('E' , 12)],
-    'C' : [('F' , 7)],
-    'D' : [],
-    'E' : [('G' , 2)],
-    'F' : [],
-    'G' : []
-    }
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['G'],
+    'F': [],
+    'G': []
+}
 
-herustic = {
-    'A' : 10,
-    'B' : 8,
-    'C' : 5,
-    'D' : 7,
-    'E' : 3,
-    'F' : 6,
-    'G' : 0
-    }
 
-def bfs(start , goal):
+def bfs(start, goal):
+
     visited = set()
-    pq = PriorityQueue()
-    pq.put((herustic[start] , start))
-    while not pq.empty():
-        h , current = pq.get()
+    queue = deque([start])
+
+    while queue:
+
+        current = queue.popleft()
+
         if current in visited:
             continue
-        print(current , end=" ")
+
+        print(current, end=" ")
+
         visited.add(current)
+
         if current == goal:
-            print("\nGoal reached")
+            print("\nGoal Reached")
             return
-        for neighbor , cost in graph[current]:
+
+        for neighbor in graph[current]:
             if neighbor not in visited:
-                pq.put((herustic[neighbor] , neighbors))
-    print("\nGoal not found")
-    
-    start_node = 'A'
-    goal_node = 'G'
-    
-    bfs(start_node , goal_node)
-        
-        
-        
-        
-        
-        
-        
+                queue.append(neighbor)
+
+    print("\nGoal Not Found")
+
+
+bfs('A', 'G')
+
+#A B C D E F G 
+#Goal Reached
